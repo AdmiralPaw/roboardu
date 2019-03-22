ccc98a909bd1474c2cc7f672cc79dff23796fae8
lerofa <romankr998@gmail.com>
2019-03-23 00:59:08 +0300

    Исправил ошибку

---
+++ src/main/java/com/ardublock/ui/СontrollerСonfiguration.java
@@ -0,0 +1,12 @@
+package com.ardublock.ui;
+
+import javax.swing.*;
+
+public class СontrollerСonfiguration extends JPanel {
+
+
+    public СontrollerСonfiguration()
+    {
+        super();
+    }
+}
