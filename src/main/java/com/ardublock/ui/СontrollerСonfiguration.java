dc7452007101146cf1e958850be103994a309783
lerofa <romankr998@gmail.com>
2019-03-26 20:14:39 +0300

    Изменен цвет рабочего стола.

--- src/main/java/com/ardublock/ui/СontrollerСonfiguration.java
+++ src/main/java/com/ardublock/ui/СontrollerСonfiguration.java
@@ -1,12 +1,15 @@
 package com.ardublock.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class СontrollerСonfiguration extends JPanel {
 
 
     public СontrollerСonfiguration()
     {
+
         super();
+        setBackground(new Color(200,200,200));
     }
 }
