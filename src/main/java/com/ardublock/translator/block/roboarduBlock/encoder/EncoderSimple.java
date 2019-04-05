32e7081e3052ec255e32eca05f183a2b5ef725a5
teamroboscratch <swaglord2010@mail.ru>
2019-04-05 18:07:02 +0300

    Даня добавил Энкодер

---
+++ src/main/java/com/ardublock/translator/block/roboarduBlock/encoder/EncoderSimple.java
@@ -0,0 +1,31 @@
+package com.ardublock.translator.block.roboarduBlock.encoder;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class EncoderSimple extends TranslatorBlock{
+    public EncoderSimple (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+    {
+        super(blockId, translator, codePrefix, codeSuffix, label);
+    }
+
+    @Override
+    public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+    {
+        String EncoderSimple="EncoderSimple";
+        String EncoderFull="EncoderFull";
+
+
+
+        translator.addDefinitionCommand("\nvoid Encoder_FUNCTION(){\n" +
+                EncoderSimple+"++;\n"+
+                EncoderFull+"="+EncoderSimple+"/20;\n"+
+                "}\n");
+        translator.addSetupCommand("attachInterrupt(2, Encoder_FUNCTION, RISING);\n");
+
+
+        return codePrefix + EncoderFull + codeSuffix;
+    }
+}
