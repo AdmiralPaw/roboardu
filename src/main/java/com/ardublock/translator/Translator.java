16620e1ab19572a22b0c95d4112dab2d217ee744
teamroboscratch <swaglord2010@mail.ru>
2019-04-12 12:58:31 +0300

    Almost finished blocks

--- src/main/java/com/ardublock/translator/Translator.java
+++ src/main/java/com/ardublock/translator/Translator.java
@@ -25,7 +25,7 @@ import edu.mit.blocks.workspace.Workspace;
 
 public class Translator
 {
-	private static final String variablePrefix = "_ABVAR_";
+	private static final String variablePrefix = "_";
 
 	private Set<String> headerFileSet;
         private Set<String> headerDefinitionSet;
--- src/main/java/com/ardublock/translator/block/roboarduBlock/sensors/rangefinderCombined.java
+++ src/main/java/com/ardublock/translator/block/roboarduBlock/sensors/rangefinderCombined.java
@@ -1,35 +1,38 @@
 package com.ardublock.translator.block.roboarduBlock.sensors;
 
-/*import com.ardublock.translator.Translator;
+import com.ardublock.translator.Translator;
 import com.ardublock.translator.block.TranslatorBlock;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
-public class EncoderSimple2 extends TranslatorBlock {
-    public EncoderSimple2(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+public class rangefinderCombined extends TranslatorBlock {
+    public rangefinderCombined(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
         super(blockId, translator, codePrefix, codeSuffix, label);
     }
 
     @Override
-    public String toCode() throws SocketNullException, SubroutineNotDeclaredException {
-        String EncoderSimple2 = translator.getNumberVariable("EncoderSimple2");
+    public String toCode() {
+        String sensor = "sensorCombined";
 
 
-        if (EncoderSimple2 == null) {
-            EncoderSimple2 = translator.buildVariableName("EncoderSimple");
-            translator.addNumberVariable("EncoderSimple2", EncoderSimple2);
-            translator.addDefinitionCommand("unsigned long long " + EncoderSimple2 + " = 0 ;");
-        }
+        translator.addHeaderDefinition("Wire.h");
+        translator.addHeaderDefinition("SparkFun_VL6180X.h");
+        translator.addDefinitionCommand("#define VL6180X_ADDRESS 0x29\n" +
+                "\n" +
+                "VL6180xIdentification identification;\n" +
+                "VL6180x " + sensor + "(VL6180X_ADDRESS);");
+        translator.addSetupCommand("Wire.begin();\n" +
+                "if(" + sensor + ".VL6180xInit() != 0){\n" +
+                "    Serial.println(\"FAILED TO INITALIZE\");\n" +
+                "  }; \n" +
+                "\n" +
+                "  " + sensor + ".VL6180xDefautSettings();\n" +
+                "  \n" +
+                "    delay(1000);");
 
-        if (translator.getNumberVariable("EncoderFull2") == null) {
-            translator.addDefinitionCommand("\nvoid Encoder_FUNCTION2(){\n" +
-                    EncoderSimple2 + "++;\n" +
-                    "}\n");
-            translator.addSetupCommand("attachInterrupt(3, Encoder_FUNCTION2, RISING);\n");
-        }
 
 
-        return codePrefix + EncoderSimple2 + codeSuffix;
+        return codePrefix + sensor + ".getDistance()" + codeSuffix;
     }
-}*/
+}
 
