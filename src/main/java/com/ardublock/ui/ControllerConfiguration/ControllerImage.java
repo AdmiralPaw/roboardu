5fa3964f316febe32895e2cda61a33d4c85debcd
teamroboscratch <swaglord2010@mail.ru>
2019-05-04 21:57:34 +0300

    to download

--- src/main/java/com/ardublock/ui/ControllerConfiguration/ControllerImage.java
+++ src/main/java/com/ardublock/ui/ControllerConfiguration/ControllerImage.java
@@ -9,24 +9,11 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.util.ArrayList;
 import com.ardublock.ui.ControllerConfiguration.InvisibleButton;
+import com.ardublock.ui.ControllerConfiguration.pins;
 
 
 public class ControllerImage extends JPanel{
-    enum pins{
-        dir04pwm05,
-        dir07pwm06,
-        d2,
-        d3,
-        d8,
-        d10,
-        d9,
-        d11,
-        a3,
-        a2,
-        a1,
-        a0,
-        i2c
-    }
+
 
     private BufferedImage image;
     private BufferedImage usingImage;
