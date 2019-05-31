b807418d14edc5e01e2479a3665d9d1308c87793
Kesha <kesha.tkachenko2017@mail.ru>
2019-05-31 15:27:47 +0300

    all ardublock refactored

--- src/main/java/com/ardublock/core/exception/ArdublockException.java
+++ src/main/java/com/ardublock/core/exception/ArdublockException.java
@@ -3,9 +3,6 @@ package com.ardublock.core.exception;
 public class ArdublockException extends Exception //RuntimeException
 {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1006562884406802985L;
 
 }
