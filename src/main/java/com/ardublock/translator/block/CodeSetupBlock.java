642c68bcc7d25380fe14f8b8f404e2a874cf5a97
Kesha <kesha.tkachenko2017@mail.ru>
2019-05-31 14:40:11 +0300

    unused deleted

--- src/main/java/com/ardublock/translator/block/CodeSetupBlock.java
+++ src/main/java/com/ardublock/translator/block/CodeSetupBlock.java
@@ -17,8 +17,6 @@ public class CodeSetupBlock extends TranslatorBlock
 		
 		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
 		String ret = translatorBlock.toCode();
-		//ret=ret.substring(1);
-		//ret=ret.replace(ret.substring(ret.length()-1),"");
 		ret=ret+"\n";
 		translator.addSetupCommandForced(ret);
 		return "";
