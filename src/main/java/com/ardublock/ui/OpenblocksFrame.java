40855c194448ea0f615e9dfc67cac8bb48d50c55
lerofa <romankr998@gmail.com>
2019-06-27 18:07:46 +0300

    Новый дизайн панели выбора констант у блоков

--- src/main/java/com/ardublock/ui/OpenblocksFrame.java
+++ src/main/java/com/ardublock/ui/OpenblocksFrame.java
@@ -1,8 +1,7 @@
 package com.ardublock.ui;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -35,8 +34,7 @@ import com.mit.blocks.workspace.SearchBar;
 import com.mit.blocks.workspace.ZoomSlider;
 import com.mit.blocks.workspace.SearchableContainer;
 import com.mit.blocks.workspace.Workspace;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
