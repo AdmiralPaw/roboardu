9136d40fb3455ee615a9a7751b1ff88bb5586277
AdmiralPaw <31961719+AdmiralPaw@users.noreply.github.com>
2019-08-28 03:16:36 +0300

    Tutorial's locations

--- src/main/java/com/ardublock/ui/TPanel.java
+++ src/main/java/com/ardublock/ui/TPanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
@@ -48,9 +49,9 @@ public class TPanel extends JPanel {
         Image imageRaw = image.getImage().getScaledInstance(
                 width / 2 - 10, 30, java.awt.Image.SCALE_SMOOTH);
         prevButton = new JButton(new ImageIcon(imageRaw)/*"< НАЗАД"*/);
-        prevButton.setFont(new Font("Impact", Font.PLAIN, 14));
-//        prevButton.setMargin(new Insets(0, 0, 0, 0));
-//        prevButton.setIconTextGap(0);
+//        prevButton.setFont(new Font("Impact", Font.PLAIN, 14));
+        prevButton.setMargin(new Insets(0, 0, 0, 0));
+        prevButton.setIconTextGap(0);
         prevButton.setBorderPainted(false);
         prevButton.setBorder(null);
         prevButton.setFocusable(false);
@@ -66,6 +67,8 @@ public class TPanel extends JPanel {
                 width / 2 - 10, 30, java.awt.Image.SCALE_SMOOTH);
         nextButton = new JButton(new ImageIcon(imageRaw)/*"ДАЛЕЕ >"*/);
 //        nextButton.setFont(new Font("Impact", Font.PLAIN, 14));
+        nextButton.setMargin(new Insets(0, 0, 0, 0));
+        nextButton.setIconTextGap(0);
         nextButton.setBorderPainted(false);
         nextButton.setBorder(null);
         nextButton.setFocusable(true);
@@ -96,7 +99,7 @@ public class TPanel extends JPanel {
 
         prevButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (tutorialPane.iter < tutorialPane.activeAnimPanels.size()) {
+                if (tutorialPane.iter > -1) {
                     if (tutorialPane.activeAnimPanels.get(tutorialPane.iter).get(0).animationIsFinished) {
                         if (!tutorialPane.tutorIllumPanels.get(tutorialPane.iter).get(0).animationIsFinished) {
                             for (IllumPanel pane : tutorialPane.tutorIllumPanels.get(tutorialPane.iter)) {
@@ -116,14 +119,33 @@ public class TPanel extends JPanel {
         this.tutorialText.setText(newText);
     }
 
-    public void changeLocation(int x, int y) {
-        this.setLocation(x, y);
-        this.getParent().repaint();
+    public void changeLocation(Point p) {
+        this.setLocation(p.x, p.y);
+        this.repaint();
     }
 
-    public void changeDimension(int width, int height) {
-        this.setSize(width, height);
-        this.getParent().repaint();
+    public void changeDimension(Dimension d) {
+        this.width = d.width;
+        this.height = d.height;
+        
+        this.setSize(new Dimension(width, height));
+        this.repaint();
+        
+        tutorialText.setSize(new Dimension(width, height - 40));
+        tutorialText.setPreferredSize(new Dimension(width, height - 40));
+        tutorialText.repaint();
+        
+        prevButton.setSize(new Dimension(width / 2 - 10, 30));
+        prevButton.setPreferredSize(new Dimension(width / 2 - 10, 30));
+        prevButton.setLocation(0, this.tutorialText.getHeight()/*height - 40*/);
+        prevButton.repaint();
+        
+        nextButton.setSize(new Dimension(width / 2 - 10, 30));
+        nextButton.setPreferredSize(new Dimension(width / 2 - 10, 30));
+        nextButton.setLocation(width / 2, this.tutorialText.getHeight()/*height - 40*/);
+        nextButton.repaint();
+        
+        this.updateUI();
     }
 
     public void setTextColor(Color newColor) {
