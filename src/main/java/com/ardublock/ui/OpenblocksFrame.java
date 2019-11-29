574415e8df7b70fde056567545a6d8c6691a51b3
JesusProfile <epicycloida@yandex.ru>
2019-11-29 15:14:40 +0300

    Мелочи

--- src/main/java/com/ardublock/ui/OpenblocksFrame.java
+++ src/main/java/com/ardublock/ui/OpenblocksFrame.java
@@ -575,7 +575,7 @@ public class OpenblocksFrame extends JFrame {
                 URL url;
                 if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
                     try {
-                        url = new URL("http://ardublock.com");
+                        url = new URL("https://omegabot.ru/");
                         desktop.browse(url.toURI());
                     } catch (Exception e1) {
                         e1.printStackTrace();
