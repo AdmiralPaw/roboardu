f4579b7f2dbfefe3a499657391a3f4e53d6dd3ed
AdmiralPaw <31961719+AdmiralPaw@users.noreply.github.com>
2020-04-27 22:53:20 +0300

    [ADD] Engine_RightTurnTime
    Добавил поворот на право по времени.
    Нужен перевод.

---
+++ src/main/java/com/ardublock/translator/block/roboarduBlock/Engine/Engine_RightTurnTime.java
@@ -0,0 +1,51 @@
+package com.ardublock.translator.block.roboarduBlock.Engine;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+/**
+ *
+ * @author User
+ */
+public class Engine_RightTurnTime extends TranslatorBlock {
+
+    /**
+     *
+     * @param blockId
+     * @param translator
+     * @param codePrefix
+     * @param codeSuffix
+     * @param label
+     */
+    public Engine_RightTurnTime(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+        super(blockId, translator, codePrefix, codeSuffix, label);
+    }
+
+    /**
+     *
+     * @return
+     * @throws SocketNullException
+     * @throws SubroutineNotDeclaredException
+     */
+    @Override
+    public String toCode() throws SocketNullException, SubroutineNotDeclaredException {
+        translator.CheckClassName(this);
+
+        translator.addSetupCommand("InitMotors();");
+
+        TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+        String val = translatorBlock.toCode();
+        if (Double.parseDouble(val) > 100 || Double.parseDouble(val) < -100) {
+            throw new BlockException(translatorBlock.getBlockID(), "ARGUMENT_ERROR");
+        };
+        String ret = "MoveRightByDelay(" + translatorBlock.toCode() + ", ";
+
+        translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+        ret = ret + translatorBlock.toCode() + ");";
+
+        return codePrefix + ret + codeSuffix;
+    }
+}
