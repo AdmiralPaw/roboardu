d288056aa31f54a860b6e1bb387c1c5bfdac3abb
Ritevi <sanekv1999@gmail.com>
2020-04-28 00:39:21 +0300

    [ADD] LedBrightness name pwmLed

--- src/main/java/com/ardublock/translator/block/roboarduBlock/blink_ledD13.java
+++ src/main/java/com/ardublock/translator/block/roboarduBlock/blink_ledD13.java
@@ -49,7 +49,7 @@ public class blink_ledD13 extends TranslatorBlock {
         tb = this.getRequiredTranslatorBlockAtSocket(1);
         String TimeBlink = tb.toCode();
         if((Integer.parseInt(TimeBlink)<1) || (Integer.parseInt(TimeBlink)>10000)){
-        throw new BlockException(tb.getBlockID(), "ARGUMENT_ERROR");
+            throw new BlockException(tb.getBlockID(), "ARGUMENT_ERROR");
         }
  
         translator.LoadTranslators(this.getClass().getSimpleName());
