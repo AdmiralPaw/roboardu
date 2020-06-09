a7c5dbe7f2f186bd4e1538c660d15a40fcb6b48a
AdmiralPaw <31961719+AdmiralPaw@users.noreply.github.com>
2020-06-09 19:59:14 +0300

    [BUG] Закрыт
    Все блоки из категории "звук" имеют нормальные сообщения об ошибках и ограничения ввода

--- src/main/java/com/ardublock/translator/block/roboarduBlock/ToneTime.java
+++ src/main/java/com/ardublock/translator/block/roboarduBlock/ToneTime.java
@@ -11,57 +11,59 @@ import com.ardublock.translator.block.TranslatorBlock;
 import com.ardublock.translator.block.exception.BlockException;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
 import java.util.ResourceBundle;
 
 /**
- *
  * @author User
  */
-public class ToneTime extends TranslatorBlock
-{
+public class ToneTime extends TranslatorBlock {
     private static ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
 
     /**
-     *
      * @param blockId
      * @param translator
      * @param codePrefix
      * @param codeSuffix
      * @param label
      */
-    public ToneTime(Long blockId, Translator translator, String codePrefix,	String codeSuffix, String label)
-	{
-		super(blockId, translator, codePrefix, codeSuffix, label);
-	}
-        
+    public ToneTime(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+        super(blockId, translator, codePrefix, codeSuffix, label);
+    }
+
     /**
      *
      */
 
     /**
-     *
      * @return
      * @throws SocketNullException
      * @throws SubroutineNotDeclaredException
      */
     @Override
-	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
-	{
-		TranslatorBlock pinBlock = this.getRequiredTranslatorBlockAtSocket(0);
-                 if(!("A0 A1 A2 A3 8 9 10 11 12").contains(pinBlock.toCode().trim())) {
-                    throw new BlockException(blockId, uiMessageBundle.getString("ardublock.error_msg.Digital_pin_slot"));
-                }
-		TranslatorBlock freqBlock = this.getRequiredTranslatorBlockAtSocket(1);
-                if((Integer.parseInt(freqBlock.toCode())<100) || (Integer.parseInt(freqBlock.toCode())>3000)){
-                    throw new BlockException(freqBlock.getBlockID(), "ARGUMENT_ERROR");
-                }
-		TranslatorBlock timeBlock = this.getRequiredTranslatorBlockAtSocket(2);
-                if((Integer.parseInt(timeBlock.toCode())<1) || (Integer.parseInt(timeBlock.toCode())>10)){
-                    throw new BlockException(timeBlock.getBlockID(), "ARGUMENT_ERROR");
-                }
-                translator.LoadTranslators(this.getClass().getSimpleName());
-		
-		String ret = "PiezoTone(" + pinBlock.toCode() + ", " + freqBlock.toCode() + ", " + timeBlock.toCode() + ");\n";
-		return ret;
-	}
+    public String toCode() throws SocketNullException, SubroutineNotDeclaredException {
+        TranslatorBlock pinBlock = this.getRequiredTranslatorBlockAtSocket(0);
+        if (!("A0 A1 A2 A3 8 9 10 11 12").contains(pinBlock.toCode().trim())) {
+            throw new BlockException(blockId, uiMessageBundle.getString("ardublock.error_msg.Digital_pin_slot"));
+        }
+        TranslatorBlock freqBlock = this.getRequiredTranslatorBlockAtSocket(1);
+        int leftLimit = 100;
+        int rightLimit = 4000;
+        //TODO может засунуть строку ошибки в базу данных?
+        if ((Integer.parseInt(freqBlock.toCode()) < leftLimit) || (Integer.parseInt(freqBlock.toCode()) > rightLimit)) {
+            throw new BlockException(freqBlock.getBlockID(), "[Ошибка в значении] Частота может быть только от "
+                    + leftLimit + " до " + rightLimit);
+        }
+        TranslatorBlock timeBlock = this.getRequiredTranslatorBlockAtSocket(2);
+        leftLimit = 1;
+        rightLimit = 10;
+        if ((Integer.parseInt(timeBlock.toCode()) < 1) || (Integer.parseInt(timeBlock.toCode()) > 10)) {
+            throw new BlockException(timeBlock.getBlockID(), "[Ошибка в значении] Значение секунд может быть только от "
+                    + leftLimit + " до " + rightLimit);
+        }
+        translator.LoadTranslators(this.getClass().getSimpleName());
+
+        String ret = "PiezoTone(" + pinBlock.toCode() + ", " + freqBlock.toCode() + ", " + timeBlock.toCode() + ");\n";
+        return ret;
+    }
 }
